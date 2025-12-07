2a73f945a97f4eb9e08db14ea7a95d0a9290afc9
UjjawalGusain <ujjawalgusain31@gmail.com>
2025-12-07 22:12:18 +0530

    Bug fix: relative route import

--- backend/src/routes/query.route.ts
+++ backend/src/routes/query.route.ts
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import QueryController from "src/controllers/query.controller.js";
+import QueryController from "./../controllers/query.controller.js";
 
 const router = Router();
 router.get("/", QueryController.queryTransactions);
