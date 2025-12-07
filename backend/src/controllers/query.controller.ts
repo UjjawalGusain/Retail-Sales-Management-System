077eda04ba42a8bd036358ff690b6e9099a81155
UjjawalGusain <ujjawalgusain31@gmail.com>
2025-12-07 22:24:47 +0530

    Bog fix: import error

--- backend/src/controllers/query.controller.ts
+++ backend/src/controllers/query.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { Prisma } from "./../generated/prisma/client.js"
-import prisma from "./../services/prismaClient.js"
+import { Prisma } from "./../generated/prisma/client"
+import prisma from "./../services/prismaClient"
 import type {
     TransactionsApiResponse,
     ApiErrorResponse,
